61322d292a4261b106a2caed189c6a14738113dc
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-29 01:10:16 +0530

    Create AdminPrivate.jsx

---
+++ client/src/Routes/AdminPrivate.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useAuth } from '../Context/Auth'
+import { Outlet } from 'react-router-dom'
+import Spinner from '../Components/Spinner'
+// import axios from 'axios'
+// import Spinner from '../Components/Spinner'
+
+
+const AdminRoute = () => {
+  const [ok,setOk] = useState(false)
+  const [auth]= useAuth()
+useEffect(()=>{
+  
+
+
+  if(auth?.token && auth.user.role === 1){
+    setOk(true)
+  }
+
+
+},[auth?.token])
+
+return ok ? <Outlet/> :<Spinner path='/login'/>
+}
+
+export default AdminRoute
