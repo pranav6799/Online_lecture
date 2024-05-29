035da2ac41ffe9643efa6c83def2966b1b5fb6f0
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-29 09:39:04 +0530

    Update instructorList.jsx

--- client/src/components/instructorList.jsx
+++ client/src/components/instructorList.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { useAuth } from "../Context/Auth"
+import { useAuth } from "../context/Auth"
 import { useState,useEffect} from "react"
 import toast from "react-hot-toast"
 import { useNavigate } from "react-router-dom";
