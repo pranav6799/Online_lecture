bf39119b1bd6e2edc6be5f3765f839ef0ad3d7c9
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-29 09:05:24 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./Context/Auth.jsx";
+import { AuthProvider } from "../src/Context/Auth.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <AuthProvider>
