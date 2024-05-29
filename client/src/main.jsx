13616f97fa99e6f44845c15cf3535b47879e39ef
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-29 10:27:04 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+window.React= React
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
