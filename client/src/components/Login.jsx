c44790d1bcb261b385c3400095def3419603e40a
pranav6799 <150272347+pranav6799@users.noreply.github.com>
2024-05-29 09:36:36 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import {useNavigate} from 'react-router-dom'
-import { useAuth } from '../Context/Auth'
+import { useAuth } from '../context/Auth'
 import { Toaster } from 'react-hot-toast';
 
 
